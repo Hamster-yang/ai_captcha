903ef9d739c18aaa2a92cc1ef60b0d77f06ae586
allen <allenhke@gmail.com>
2022-12-27 21:53:48 +0800

    v8.0

--- school_captcha.py
+++ school_captcha.py
@@ -15,6 +15,7 @@ from keras.models import load_model
 from keras import callbacks
 import string
 
+
 # 设置putText函数字体
 font=cv.FONT_HERSHEY_SIMPLEX
 #计算两边夹角额cos值
@@ -41,7 +42,7 @@ def predict(filepath):
     img=cv.resize(img,[300,100])
     kernel = np.ones((3,3), np.uint8)
     img=cv.dilate(img,kernel,iterations = 1)
-    ret,img=cv.threshold(img,220,255,cv.THRESH_BINARY)
+    ret,img=cv.threshold(img,203,255,cv.THRESH_BINARY)
 
     img=cv.resize(img,[120,30])
 
@@ -51,7 +52,6 @@ def predict(filepath):
         print("Not detected")
         
         return '100000'
-    
     res = np.array(model.predict(img[np.newaxis, :, :, np.newaxis],verbose=0))
     ans = np.reshape(res, (5, 62))#-------------------------------------------------------
     l_ind = []
@@ -59,7 +59,7 @@ def predict(filepath):
     for a in ans:
         l_ind.append(np.argmax(a))
         #probs.append(np.max(a))
-
+    
     capt = ''
     for l in l_ind:
         capt += symbols[l]
@@ -75,12 +75,11 @@ def angle_cos(p0, p1, p2):
 def find_squares(img_org):
     squares = []
     img = cv.GaussianBlur(img_org, (3, 3), 0)   
+    
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     bin = cv.Canny(gray, 30, 100, apertureSize=3) 
-    #cv.imshow("123", bin)
-    #cv.waitKey()   
     contours, _hierarchy = cv.findContours(bin, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    print("輪廓数量：%d" % len(contours))
+    #print("輪廓数量：%d" % len(contours))
     index = 0
     # 輪廓
     for cnt in contours:
@@ -88,9 +87,8 @@ def find_squares(img_org):
         cnt = cv.approxPolyDP(cnt, 0.02*cnt_len, True) #多邊形逼近
         #print(cnt_len)
         # 条件判断逼近边的数量是否为4，輪廓面积是否大于7000，检测轮廓是否为凸的
-        if len(cnt) == 4 and cv.contourArea(cnt) > 3000 and cv.isContourConvex(cnt): #and cv.contourArea(cnt) < 5000  len(cnt) == 4 and   and cv.isContourConvex(cnt)
-            print("---------------------------------------------------------------------------")
-            print(cv.contourArea(cnt))
+        if len(cnt) == 4 and cv.contourArea(cnt) > 5600 and cv.contourArea(cnt) < 5650  and cv.isContourConvex(cnt): #and cv.contourArea(cnt) < 5000  len(cnt) == 4 and   and cv.isContourConvex(cnt)
+            
             M = cv.moments(cnt) #计算轮廓的矩
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])#輪廓数量
@@ -107,12 +105,13 @@ def find_squares(img_org):
             y = cnt[0][1]
 
             # 裁切區域的長度與寬度
-            w = 120
-            h = 30
+            w = 150
+            h = 37
 
             # 裁切圖片 
             
-            crop_img = img_org[y:y+h, x:x+w]
+            crop_img = img_org[y-1:y+h, x+1:x+w]
+            crop_img=cv.resize(crop_img,[120,30])
             find = True
             # 只检测矩形（cos90° = 0）
             #if max_cos < 0.1:
@@ -128,7 +127,7 @@ def find_squares(img_org):
     return img, squares, img, find
 
 def main():
-    
+    ago_filename=''
     output_folder = './output/'
     while True:
         # 全螢幕擷取
@@ -137,17 +136,21 @@ def main():
         
         cv.imshow('imm', img_bgr)
         name = '{}full_screen.png'.format(output_folder)
-        print(img_bgr.shape)
         cv.imwrite(name, img_bgr)
         crop_img, squares, img, find = find_squares(img_bgr)
         if find == True:
+            
             file_name = '{}test.png'.format(output_folder)
             cv.imwrite(file_name, crop_img)
-            img_predict = cv.imread('{}test.png'.format(output_folder))
-
             
-            print(predict(file_name))
-            cv.waitKey()
+            captcha_name=predict(file_name)
+            
+            
+            if ago_filename== captcha_name:
+                ago_filename=captcha_name
+            else:    
+                print(captcha_name)
+                ago_filename=captcha_name
         
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
